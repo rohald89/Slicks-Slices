ae8a6773d20f0f1afeb3e924081accbba41f2f94
Rohald van Merode <rohald_89@live.nl>
2021-03-17 21:24:45 +0100

    added styles

--- gatsby/.cache/api-runner-ssr.js
+++ gatsby/.cache/api-runner-ssr.js
@@ -1,4 +1,7 @@
-var plugins = []
+var plugins = [{
+      plugin: require('/Users/rohald/OneDrive/Documents/Projects/Master-Gatsby/gatsby/gatsby-ssr'),
+      options: {"plugins":[]},
+    }]
 // During bootstrap, we write requires at top of this file which looks like:
 // var plugins = [
 //   {
