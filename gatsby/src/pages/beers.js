fbba4cbe4572e90b0935bed78bde61bed7aa5bd5
Rohald van Merode <rohald_89@live.nl>
2021-03-21 12:41:13 +0100

    added beers and slicemasters pages including styling and fetching the data

--- gatsby/src/pages/beers.js
+++ gatsby/src/pages/beers.js
@@ -1,9 +1,73 @@
+import { graphql } from 'gatsby';
 import React from 'react';
+import styled from 'styled-components';
 
-const BeersPage = () => (
+const BeerGridStyles = styled.div`
+  display: grid;
+  gap: 2rem;
+  grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
+`;
+
+const SingleBeerStyles = styled.div`
+  border: 1px solid var(--grey);
+  padding: 2rem;
+  text-align: center;
+  img {
+    width: 100%;
+    height: 200px;
+    object-fit: contain;
+    display: grid;
+    align-items: center;
+    font-size: 10px;
+  }
+  .grey-stars {
+    filter: grayscale(1);
+  }
+`;
+
+const BeersPage = ({ data }) => {
+  console.log(data);
+  return (
     <>
-        <p>Hey! I'm the Beers page</p>
+      <h2 className="center">
+        We have {data.beers.nodes.length} Beers available. Dine in Only!
+      </h2>
+      <BeerGridStyles>
+        {data.beers.nodes.map((beer) => {
+          const rating = Math.round(beer.rating.average);
+          return (
+            <SingleBeerStyles key={beer.id}>
+              <img src={beer.image} alt={beer.name} />
+              <h3>{beer.name}</h3>
+              {beer.price}
+              <p title={`${rating} out of 5 stars`}>
+                {`⭐`.repeat(rating)}
+                <span className="grey-stars">{`⭐`.repeat(5 - rating)}</span>
+                <span>({beer.rating.reviews})</span>
+              </p>
+            </SingleBeerStyles>
+          );
+        })}
+      </BeerGridStyles>
     </>
-)
+  );
+};
 
 export default BeersPage;
+
+export const query = graphql`
+  query {
+    beers: allBeer {
+      nodes {
+        id
+        name
+        price
+        image
+        rating {
+          average
+          reviews
+        }
+      }
+    }
+  }
+`;
