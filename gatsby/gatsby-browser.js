b362f1666ed219cfc029857ac72e7ea0e1615a03
Rohald van Merode <rohald_89@live.nl>
2021-03-21 21:20:45 +0100

    added the order state to the Root so the order is still present when changing the page

--- gatsby/gatsby-browser.js
+++ gatsby/gatsby-browser.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import Layout from './src/components/Layout';
+import { OrderProvider } from './src/components/OrderContext';
 
 const wrapPageElement = ({ element, props }) => (
-    <Layout {...props}>{ element } </Layout>
-)
+  <Layout {...props}>{element} </Layout>
+);
+const wrapRootElement = ({ element }) => (
+  <OrderProvider>{element}</OrderProvider>
+);
 
-export {wrapPageElement};
+export { wrapPageElement, wrapRootElement };
--- gatsby/gatsby-ssr.js
+++ gatsby/gatsby-ssr.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import Layout from './src/components/Layout';
+import { OrderProvider } from './src/components/OrderContext';
 
 const wrapPageElement = ({ element, props }) => (
-    <Layout {...props}>{ element } </Layout>
-)
-
-export {wrapPageElement};
+  <Layout {...props}>{element} </Layout>
+);
+const wrapRootElement = ({ element }) => (
+  <OrderProvider>{element}</OrderProvider>
+);
+export { wrapPageElement, wrapRootElement };
