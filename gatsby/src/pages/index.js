ac457a99f53487d73fb11bc686634b0d876c397f
Rohald van Merode <rohald_89@live.nl>
2021-03-27 14:21:47 +0100

    added client side data fetching for the home page

--- gatsby/src/pages/index.js
+++ gatsby/src/pages/index.js
@@ -1,9 +1,33 @@
 import React from 'react';
+import useLatestData from '../utils/useLatestData';
 
-const HomePage = () => (
-    <>
-        <p>Hey! I'm the home page</p>
-    </>
-)
+function CurrentlySlicing() {
+  return (
+    <div>
+      <p>CurrentlySlicing</p>
+    </div>
+  );
+}
+function HotSlices() {
+  return (
+    <div>
+      <p>HotSlices</p>
+    </div>
+  );
+}
+
+const HomePage = () => {
+  const { slicemasters, hotSlices } = useLatestData();
+  return (
+    <div className="center">
+      <h1>The Best Pizza Downtown!</h1>
+      <p>Open 11am to 11pm Every Single Day</p>
+      <div>
+        <CurrentlySlicing slicemasters={slicemasters} />
+        <HotSlices hotSlices={hotSlices} />
+      </div>
+    </div>
+  );
+};
 
 export default HomePage;
