124f2c1421aa42de421d327af9fb93debacad00f
Rohald van Merode <rohald_89@live.nl>
2021-03-29 22:20:09 +0200

    added css prefixes

--- gatsby/src/styles/MenuItemStyles.js
+++ gatsby/src/styles/MenuItemStyles.js
@@ -1,11 +1,17 @@
 import styled from 'styled-components';
 
 const MenuItemStyles = styled.div`
+  display: -ms-grid;
   display: grid;
+  -ms-grid-columns: 100px 1.3rem 1fr;
   grid-template-columns: 100px 1fr;
+  -ms-grid-rows: 1fr 0 1fr;
   grid-template-rows: 1fr 1fr;
   gap: 0 1.3rem;
+  -ms-flex-line-pack: center;
   align-content: center;
+  -webkit-box-align: center;
+  -ms-flex-align: center;
   align-items: center;
   position: relative;
   .gatsby-image-wrapper {
